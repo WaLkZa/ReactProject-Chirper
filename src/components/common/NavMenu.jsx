9aa4d1d2790987092b976e4d12ba026b2f2c9a39
WaLkZa <walkzi@yandex.com>
2018-07-10 21:00:57 +0300

    added color for active link in navigation bar

--- src/components/common/NavMenu.jsx
+++ src/components/common/NavMenu.jsx
@@ -6,10 +6,10 @@ class NavMenu extends Component {
     render() {
         return (
             <div className="menu">
-                <NavLink to="/feed">Home</NavLink>
-                <NavLink to="/discover">Discover</NavLink>
-                <NavLink to="/profile">Me</NavLink>
-                {authService.isAdmin() && <NavLink to="/admin">Admin panel</NavLink>}
+                <NavLink to="/feed" activeClassName="nav-active">Home</NavLink>
+                <NavLink to="/discover" activeClassName="nav-active">Discover</NavLink>
+                <NavLink to="/profile" activeClassName="nav-active">Me</NavLink>
+                {authService.isAdmin() && <NavLink to="/admin" activeClassName="nav-active">Admin panel</NavLink>}
                 <NavLink to="/logout">Logout</NavLink>
             </div>
         )
