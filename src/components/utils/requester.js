33a77eecdf84871b469471b5d1a9d28bf03143f7
Simeon Nikolov <walkzi@yandex.com>
2020-05-15 18:55:31 +0300

    reworked ajax requester and auth service

--- src/components/utils/requester.js
+++ src/components/utils/requester.js
@@ -1,32 +1,22 @@
 import $ from 'jquery'
 
-//MyReactApp
-const kinveyBaseUrl = "https://baas.kinvey.com/";
-const kinveyAppKey = "kid_S1MVEYqMQ";
-const kinveyAppSecret = "8546d0afc25c48a19153f0ae2c6374f7";
-const kinveyMasterSecret = 'ea1698304ab4454ea94217e7c7523d6e';
+const baseUrl = 'http://localhost:3001/api/';
 
-// Creates the authentication header
-function makeAuth(type) {    
-    if (type === 'basic') {
-        return 'Basic ' + btoa(kinveyAppKey + ':' + kinveyAppSecret)
-    } else if (type === 'kinvey') {
-        return 'Kinvey ' + localStorage.getItem('authtoken')
-    } else if (type === 'master') {
-        return 'Basic ' + btoa(kinveyAppKey + ':' + kinveyMasterSecret)
-    }
-}
-
-// Creates request object to kinvey
 function makeRequest(method, module, endpoint, auth) {
-    return {
+    let requestObj = {
         method,
-        url: kinveyBaseUrl + module + '/' + kinveyAppKey + '/' + endpoint,
+        url: baseUrl + module + '/' + endpoint,
+        
         headers: {
-            'Authorization': makeAuth(auth),
             'Content-type': 'application/json'
         }
     };
+
+    if (auth !== 'basic') {
+        requestObj['headers']['Authorization'] = `Bearer ${localStorage.getItem('authtoken')}`;
+    }
+
+    return requestObj;
 }
 
 // Function to return GET promise
--- src/components/utils/services/authService.js
+++ src/components/utils/services/authService.js
@@ -1,7 +1,5 @@
 import requester from "../requester";
 
-const ADMIN_ROLE_ID = '5275abc5-fdbb-455e-ac27-a4b3e53a8ce1'
-
 function isAuth() {
     return localStorage.getItem('authtoken') !== null;
 }
@@ -24,11 +22,10 @@ function login(username, password) {
 function register(username, password) {
     let userData = {
         username,
-        password,
-        subscriptions: []
+        password
     };
 
-    return requester.post('user', '', 'basic', userData);
+    return requester.post('user', 'register', 'basic', userData);
 }
 
 // user/logout
@@ -42,20 +39,15 @@ function logout() {
 
 // saveSession in localStorage
 function saveSession(userInfo) {
-    let userAuth = userInfo._kmd.authtoken;
-    localStorage.setItem('authtoken', userAuth);
-    let username = userInfo.username;
-    localStorage.setItem('username', username);
-    localStorage.setItem('userId', userInfo._id);
-    localStorage.setItem('subscriptions', JSON.stringify(userInfo.subscriptions));
+    const {
+        token,
+        username,
+        userId
+    } = userInfo;
 
-    if (userInfo._kmd.roles) {
-        for (let userRole of userInfo._kmd.roles) {
-            if (userRole.roleId === ADMIN_ROLE_ID) {
-                localStorage.setItem('roleId', ADMIN_ROLE_ID)
-            }
-        }
-    }
+    localStorage.setItem('authtoken', token);
+    localStorage.setItem('username', username);
+    localStorage.setItem('userId', userId);
 }
 
 
