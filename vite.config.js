0d50cbdf6592312df311f967424aabd8bd96664d
Simeon Nikolov <simeon.nikolov@brainstarstech.com>
2025-04-13 18:40:04 +0300

    add PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['public/icons/favicon.svg', 'robots.txt', 'public/icons/apple-touch-icon.png'],
+      manifest: {
+        name: 'Chirper PWA App',
+        short_name: 'VitePWA',
+        description: 'React app with PWA support using Vite',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'public/icons/web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'public/icons/web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
